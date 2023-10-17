a5e9864974ed2125bc96c2e69bd6b99d295e007b
Marco Descher <marco@descher.at>
2023-10-17 18:37:06 +0200

    add docker base

---
+++ target/docker/build-and-push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker pull maven:3.8.7-eclipse-temurin-17-focal
+docker build --platform linux/amd64 -t gitlab.medelexis.ch:4567/elexis/docker-build:2023-03-java17 -f Dockerfile-build .
+docker push gitlab.medelexis.ch:4567/elexis/docker-build:2023-03-java17

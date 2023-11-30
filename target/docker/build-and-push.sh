85b25e824592ef1aa0ef42f8aedc401df80d0db2
Marco Descher <marco@descher.at>
2023-11-30 14:23:43 +0100

    revert mysql to 8.0.22

--- target/docker/build-and-push.sh
+++ target/docker/build-and-push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker pull maven:3.8.7-eclipse-temurin-17-focal
-docker build --platform linux/amd64 -t gitlab.medelexis.ch:4567/elexis/docker-build:2023-03-java17 -f Dockerfile-build .
-docker push gitlab.medelexis.ch:4567/elexis/docker-build:2023-03-java17
+#TODO publish to github?
+docker pull maven:3.8-eclipse-temurin-17-focal
+docker build --platform linux/amd64 -t gitlab.medelexis.ch:4567/elexis/docker-build:2023-09-java17 -f Dockerfile-build .
+docker push gitlab.medelexis.ch:4567/elexis/docker-build:2023-09-java17

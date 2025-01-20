a4cd3060fddb7e82bd5579d9116bd7e012039ec8
Marco Descher <marco@descher.at>
2025-01-20 12:05:56 +0100

    update docker build-and-push.sh

--- target/docker/build-and-push.sh
+++ target/docker/build-and-push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull maven:3.8.7-eclipse-temurin-17-focal
-docker build --platform linux/amd64 -t gitlab.medelexis.ch:4567/elexis/docker-build:2022-09-java17 -f Dockerfile-build .
-docker push gitlab.medelexis.ch:4567/elexis/docker-build:2022-09-java17
+docker build --platform linux/amd64 -t ghcr.io/elexis/docker-build:2022-09-java17 -f Dockerfile-build .
+docker push ghcr.io/elexis/docker-build:2022-09-java17
